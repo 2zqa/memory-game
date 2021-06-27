c7cfaf95b26ddc3f80d978c0dc1a8081bac11278
2zqa <25235249+2zqa@users.noreply.github.com>
2021-06-27 02:00:31 +0200

    Update header and timing service

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,12 +8,13 @@ import {BoardService} from "../board/board.service";
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  time = 0;
-  progress = 10;
+  elapsedTime = 0;
+  peekTimeRemaining = 10;
   foundCardPairs = 0;
 
   constructor(public timingService: TimingService, private boardService: BoardService) {
-    timingService.progress.subscribe(progress => this.progress = progress);
+    timingService.peekTimeRemaining.subscribe(peekTimeRemaining => this.peekTimeRemaining = peekTimeRemaining);
+    timingService.elapsedTime.subscribe(elapsedTime => this.elapsedTime = elapsedTime);
     boardService.foundCardPairs.subscribe(foundCardPairs => this.foundCardPairs = foundCardPairs);
   }
 
