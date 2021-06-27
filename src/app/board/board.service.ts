e97bf42648c8b296b751dff5bd8a334a52fe5b81
2zqa <25235249+2zqa@users.noreply.github.com>
2021-06-27 02:09:43 +0200

    Update board.service.ts

--- src/app/board/board.service.ts
+++ src/app/board/board.service.ts
@@ -2,12 +2,14 @@ import {Injectable} from '@angular/core';
 import {BehaviorSubject} from "rxjs";
 import {Card, CardStatus} from "./card";
 import {TimingService} from "../timing.service";
+import {HighscoreService} from "../highscore.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoardService {
 
+  // TODO: This isn't configurable in the settings, but has great potential to be. Maybe if we got time remaining for development.
   static readonly MAX_OPEN_CARDS = 2;
 
   private openCards: Card[] = [];
@@ -19,7 +21,9 @@ export class BoardService {
   private foundCardPairsSrc = new BehaviorSubject<number>(0);
   foundCardPairs = this.foundCardPairsSrc.asObservable();
 
-  constructor(private timingService: TimingService) {
+  maxCardPairsToFind = 0;
+
+  constructor(private timingService: TimingService, private highscoreService: HighscoreService) {
   }
 
   updateChar(char: string) {
@@ -27,7 +31,12 @@ export class BoardService {
   }
 
   newGame(size: number) {
-    this.timingService.reset();
+    this.reset();
+    this.timingService.prepareNewGame();
+    this.fillBoard(size);
+  }
+
+  fillBoard(size: number) {
     let nextLetterFunction = this.nextLetter(size);
 
     let cards: Card[][] = [];
@@ -38,12 +47,22 @@ export class BoardService {
         cards[y][x] = new Card(nextLetterFunction());
       }
     }
-
     this.cardsSrc.next(cards);
+    this.maxCardPairsToFind = size * size / BoardService.MAX_OPEN_CARDS; // bv: bord van 4x4 = 16, 2 kaarten, dus 8 paren
+  }
+
+  public generateAlphabetString(count: number): string {
+    let alphabetArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+    let newArray = [];
+    for (let letter of alphabetArray) {
+      newArray.push(letter.repeat(count));
+    }
+    return newArray.join('');
   }
 
   private nextLetter(size: number): any {
-    let letterArray = 'AABBCCDDEEFFGGHHIIJJKKLLMMNNOOPPQQRRSSTTUUVVWWXXYYZZ'.substring(0, size * size).split('');
+    let letterArray = this.generateAlphabetString(BoardService.MAX_OPEN_CARDS).substring(0, size * size).split('');
     let idx = 0;
     letterArray = BoardService.shuffle(letterArray);
     return () => {
@@ -52,6 +71,7 @@ export class BoardService {
   }
 
   cardClicked(card: Card): void {
+    this.timingService.startTime();
     // Check if card is already in list.
     if (this.openCards.includes(card) || card.status !== CardStatus.inactive) {
       return;
@@ -63,13 +83,14 @@ export class BoardService {
 
     if (this.openCards.length === BoardService.MAX_OPEN_CARDS) {
       this.checkCards(this.openCards);
+    } else if (this.openCards.length === 1) {
+      this.timingService.resetPeekTimer();
     }
   }
 
   private closeCardsAfterLimit() {
     if (this.openCards.length >= BoardService.MAX_OPEN_CARDS) {
       this.closeCards();
-      this.timingService.stopPeekTimer();
     }
   }
 
@@ -87,9 +108,19 @@ export class BoardService {
     if (areCardsEqual) {
       this.setCardsStatus(this.openCards, CardStatus.found);
       this.openCards = [];
-      this.foundCardPairsSrc.next(this.foundCardPairsSrc.value+1);
+      this.foundCardPairsSrc.next(this.foundCardPairsSrc.value + 1);
+
+      this.checkWin();
+
     } else {
       this.timingService.startPeekTimer(() => this.closeCards());
+    }
+  }
+
+  private checkWin() {
+    if (this.maxCardPairsToFind == this.foundCardPairsSrc.value) {
+      // Hurray, game is over!
+      this.highscoreService.addNewTime(this.timingService.stopTime());
     }
   }
 
@@ -116,5 +147,10 @@ export class BoardService {
       array[randomIndex] = temporaryValue;
     }
     return array;
+  }
+
+  private reset() {
+    this.closeCards();
+    this.foundCardPairsSrc.next(0);
   }
 }
